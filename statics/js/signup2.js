ae0dc93599d01e1da4cc37ceae16ac8f48c79b48
ganesh-Prasad-M <[ganeshm96324@gmail.com]>
2022-01-15 13:21:56 +0530

    pssw-eye

--- statics/js/signup2.js
+++ statics/js/signup2.js
@@ -10,3 +10,25 @@ function validate()
    }
    return true;
 }
+
+function visible(){
+   let eye = document.getElementById('eye')
+   let input = document.getElementById('pass-input')
+   let type = input.getAttribute('type')
+
+   if (type == "password")
+      input.setAttribute('type','text')
+   else 
+        input.setAttribute('type','password')
+}
+
+function visible1(){
+  let eye = document.getElementById('eye1')
+  let input = document.getElementById('c_pass-input')
+  let type = input.getAttribute('type')
+
+  if (type == "password")
+     input.setAttribute('type','text')
+  else 
+       input.setAttribute('type','password')
+}
