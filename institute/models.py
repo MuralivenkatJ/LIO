1d7dd5b2cdc15e32deac05f73fe65f695c8b02d7
Murali_venkat_J <muralivenkatj5401@gmail.com>
2022-01-24 15:08:20 +0530

    added the bank details of institute in the table

--- institute/models.py
+++ institute/models.py
@@ -8,6 +8,11 @@ class Institute(models.Model):
     image = models.ImageField(upload_to='institute')
     email = models.EmailField()
     website = models.CharField(max_length=50)
+    ac_no = models.CharField(max_length=20)
+    ac_name = models.CharField(max_length=50)
+    ifsc = models.CharField(max_length=20)
+    b_name = models.CharField(max_length=30)
+    b_address = models.CharField(max_length=100)
 
     def __str__(self):
         return self.i_name
